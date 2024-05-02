b14e7bdc102be7d3c0ba01484779e40d5569daa6
Ryan Dillon <ryan.dillon@bglgroup.co.uk>
2024-05-02 16:07:37 +0100

    push

---
+++ Script.js
@@ -0,0 +1,16 @@
+// Variables from DOM
+const FAQs = document.getElementsByClassName("FAQs");
+for (let i = 0; i < FAQs.length; i++) {
+  FAQs[i].addEventListener("click", changeClass);
+}
+
+function changeClass() {
+  this.classList.toggle("active");
+
+  let actives = document.getElementsByClassName("active");
+  for (let ii = 0; ii < actives.length; ii++) {
+    if (actives[ii] !== this) {
+      actives[ii].classList.remove("active");
+    }
+  }
+}
